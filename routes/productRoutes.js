e62a64c0da4a018c51acee4b35d85823286a9577
meesamriyaz-cyber <meesamriyaz@gmail.com>
2026-09-16 10:20:56 +0530

    fix: route product writes through stable image handlers

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,15 +11,19 @@ import {
   getFeaturedProducts,
   decreaseStock,
 } from "../controllers/productController.js";
+import {
+  addProductWithStableImages,
+  updateProductWithStableImages,
+} from "../controllers/productImageController.js";
 import { protectedRoute, adminRoute } from "../middleware/productMiddleware.js";
 const router = express.Router();
 router.get("/", getAllProducts);
 router.get("/featured", getFeaturedProducts);
 router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
-router.post("/addproduct", addProduct);
+router.post("/addproduct", addProductWithStableImages);
 router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
+router.put("/:id", updateProductWithStableImages);
 router.put("/:id/decrease-stock", decreaseStock);
 router.patch("/:id", protectedRoute, adminRoute, setFeaturedProduct);
 router.delete("/:id", deleteProduct);
